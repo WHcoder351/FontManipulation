d7033e2968f9a025028f00aaf0b7d428aa4c46f5
WHcoder351 <76533189+WHcoder351@users.noreply.github.com>
2021-08-02 16:02:40 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload() {
+    
+}
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(400,300);
+    video.position(50 , 300);
+
+    canvas = createCanvas(400, 400);
+    canvas.position(500, 250);
+
+    pose = ml5.poseNet(video, mL);
+    pose.on('pose', gP); 
+}
+function mL() {
+    console.log("PoseNet is Initialized");
+}
+function draw() {
+    
+}
+
+function gP(results) {
+    if (results.length>0) {
+        console.log(results);
+    }
+}
